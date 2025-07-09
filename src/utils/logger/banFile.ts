225012276683a06817332d4a6fedfc545441797b
Marcel Rösler <me@marcel.codes>
2025-07-09 09:45:08 +0200

    create a banfile that isn't cleaned up

--- src/utils/logger/banFile.ts
+++ src/utils/logger/banFile.ts
@@ -5,41 +5,54 @@ import { ensureDirExistence } from '../ensureDirExistence.js';
 const banFile =
   process.env.BAN_FILE_PATH || path.resolve(process.cwd(), 'data/banned.json');
 ensureDirExistence(banFile);
-let ipAccessData: Record<string, { count: number; lastAccess: number }> = {};
+const allBanFile = path.join(path.dirname(banFile), 'all_banned.json');
+
+let banData: Record<string, { count: number; lastAccess: number }> = {};
+let allBanData: Record<string, { count: number; lastAccess: number }> = {};
 
 function loadBanFile() {
   try {
     const data = fs.readFileSync(banFile, 'utf-8');
-    ipAccessData = JSON.parse(data);
+    banData = JSON.parse(data);
   } catch {
-    ipAccessData = {};
+    banData = {};
+  }
+
+  try {
+    const data = fs.readFileSync(allBanFile, 'utf-8');
+    allBanData = JSON.parse(data);
+  } catch {
+    allBanData = {};
   }
 }
 loadBanFile();
 
-function saveBanFile() {
-  fs.writeFileSync(banFile, JSON.stringify(ipAccessData, null, 2));
+function saveBanFile(fullSave?: boolean) {
+  fs.writeFileSync(banFile, JSON.stringify(banData, null, 2));
+  if (fullSave) {
+    fs.writeFileSync(banFile, JSON.stringify(allBanFile, null, 2));
+  }
 }
 
 const CLEANUP_INTERVAL = 60_000;
 const MAX_IDLE_TIME = 5 * 60_000;
 setInterval(() => {
   const now = Date.now();
-  for (const [ip, data] of Object.entries(ipAccessData)) {
+  for (const [ip, data] of Object.entries(banData)) {
     if (now - data.lastAccess > MAX_IDLE_TIME) {
-      delete ipAccessData[ip];
+      delete banData[ip];
     }
   }
   saveBanFile();
 }, CLEANUP_INTERVAL);
 
 export function isBanned(ip: string): boolean {
-  return ipAccessData[ip] && ipAccessData[ip].count >= 3;
+  return banData[ip] && banData[ip].count >= 3;
 }
 
-export function banIP(ip: string) {
+function createEntry(baseEntry?: { count: number; lastAccess: number }) {
   const now = Date.now();
-  const entry = ipAccessData[ip] || { count: 0, lastAccess: 0 };
+  const entry = baseEntry || { count: 0, lastAccess: 0 };
   const timeSinceLast = now - entry.lastAccess;
 
   if (timeSinceLast > 30_000) {
@@ -48,9 +61,15 @@ export function banIP(ip: string) {
 
   entry.count += 1;
   entry.lastAccess = now;
-  ipAccessData[ip] = entry;
+  return entry;
+}
 
-  saveBanFile();
+export function banIP(ip: string) {
+  const entry = createEntry(banData[ip]);
+  banData[ip] = entry;
+  allBanData[ip] = createEntry(allBanData[ip]);
+
+  saveBanFile(true);
   console.warn(`BANNED IP: ${ip}`);
   return entry;
 }
